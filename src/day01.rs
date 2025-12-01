48fab83e460a5ce77e9a58aa21fabc12e25f3b7c
Mikulex <mikulex@mikulex.com>
2025-12-01 21:20:27 +0100

    solve 01/1

--- src/day01.rs
+++ src/day01.rs
@@ -1,8 +1,43 @@
-use std::{error::Error, fs};
+use std::{
+    error::Error,
+    fs::{self},
+};
+
+#[derive(Debug)]
+struct Rot {
+    pub count: i32,
+    pub dir: String,
+}
+
+impl Rot {
+    pub fn new(line: &str) -> Self {
+        let split = line.split_at(1);
+        let count = split.1.parse().unwrap();
+        let dir = split.0.to_owned();
+        Self { count, dir }
+    }
+}
 
 pub fn part1(file_path: &str) -> Result<String, Box<dyn Error>> {
-    let _contents = fs::read_to_string(file_path)?;
-    return Ok(String::from("TODO"));
+    let mut current = 50;
+    let mut zero_count = 0;
+
+    fs::read_to_string(file_path)?
+        .lines()
+        .map(Rot::new)
+        .for_each(|r| {
+            if r.dir == "R" {
+                current += r.count;
+            } else {
+                current -= r.count;
+            }
+            current %= 100;
+
+            if current == 0 {
+                zero_count += 1;
+            }
+        });
+    return Ok(zero_count.to_string());
 }
 
 pub fn part2(file_path: &str) -> Result<String, Box<dyn Error>> {
@@ -16,7 +51,20 @@ mod tests {
 
     #[test]
     fn test_day01() {
-        let res = part1("inputs/01/test.txt").unwrap();
-        assert_eq!(res, "TODO");
+        let res = part1("inputs/01/demo.txt").unwrap();
+        assert_eq!(res, "3");
+    }
+
+    #[test]
+    fn test_rot_new() {
+        let input = "L32";
+        let res = Rot {
+            count: 32,
+            dir: "L".to_owned(),
+        };
+        let test = Rot::new(input);
+
+        assert_eq!(res.count, test.count);
+        assert_eq!(res.dir, test.dir);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ pub enum Part {
 }
 
 pub fn run(day: String, part: Part) {
-    let path = "inputs/{day}/input.txt";
+    let path = format!("inputs/{day}/input.txt");
     if day.eq("01") {
         if matches!(part, Part::FIRST | Part::BOTH) {
-            println!("{}", day01::part1(path).unwrap());
+            println!("{}", day01::part1(&path).unwrap());
         }
 
         if matches!(part, Part::SECOND | Part::BOTH) {
-            println!("{}", day01::part2(path).unwrap());
+            println!("{}", day01::part2(&path).unwrap());
         }
     }
 }
