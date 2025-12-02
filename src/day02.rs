11def0812f3e517ee51579d218cde8be6ddfee54
Mikulex <mikulex@mikulex.com>
2025-12-02 21:18:41 +0100

    solve 02/2

--- src/day02.rs
+++ src/day02.rs
@@ -3,24 +3,51 @@ use std::{error::Error, fs::read_to_string};
 pub fn part1(file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
     let res: u64 = read_to_string(file_path)?
         .split(",")
-        .map(|c: &str| c.split("-").map(|s: &str| s.parse::<u64>().unwrap()))
-        .flat_map(|mut r| {
-            (r.next().unwrap()..=r.next().unwrap())
-                .map(|s| s.to_string())
-                .filter(|s| s.len() % 2 == 0)
-                .filter(|s| {
-                    let (l, r): (&str, &str) = s.split_at(s.len() / 2);
-                    return l.eq(r);
-                })
-                .map(|s| s.parse::<u64>().unwrap())
+        .map(|c: &str| c.split("-").filter_map(|s: &str| s.parse::<u64>().ok()))
+        .flat_map(|mut r| r.next().unwrap()..=r.next().unwrap())
+        .map(|s| s.to_string())
+        .filter(|s| s.len() % 2 == 0)
+        .filter(|s| {
+            let (l, r): (&str, &str) = s.split_at(s.len() / 2);
+            return l.eq(r);
         })
+        .filter_map(|s| s.parse::<u64>().ok())
         .sum();
 
     return Ok(res.to_string());
 }
 
 pub fn part2(file_path: &str) -> Result<String, Box<dyn Error>> {
-    todo!();
+    let res: u64 = read_to_string(file_path)?
+        .split(",")
+        .map(|c: &str| c.split("-").filter_map(|s: &str| s.parse::<u64>().ok()))
+        .flat_map(|mut r| r.next().unwrap()..=r.next().unwrap())
+        .map(|s| s.to_string())
+        .filter(|s| {
+            let mut res = false;
+            for i in 1..=s.len() / 2 {
+                if s.len() % i == 0 {
+                    let string = s.chars().collect::<Vec<char>>();
+
+                    let mut chunks = string
+                        .chunks(i)
+                        .map(|v| v.iter().collect::<String>())
+                        .peekable();
+                    let first = chunks.peek().unwrap().clone();
+                    if chunks.all(|c| c.eq(&first.to_owned())) {
+                        res = true;
+                        break;
+                    }
+                } else {
+                    res = false;
+                }
+            }
+            res
+        })
+        .filter_map(|s| s.parse::<u64>().ok())
+        .sum();
+
+    return Ok(res.to_string());
 }
 
 #[cfg(test)]
@@ -28,8 +55,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day02() {
+    fn test_day02_01() {
         let res = part1("inputs/02/demo.txt").unwrap();
         assert_eq!(res, "1227775554");
+    }
+
+    #[test]
+    fn test_day02_02_single() {
+        let res = part2("inputs/02/single.txt").unwrap();
+        assert_eq!(res, "210");
+    }
+
+    #[test]
+    fn test_day02_02_demo() {
+        let res = part2("inputs/02/demo.txt").unwrap();
+        assert_eq!(res, "4174379265");
     }
 }
