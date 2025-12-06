4a98192377828eed604686d866883792dcdc9f1b
Mikulex <mikulex@mikulex.com>
2025-12-06 15:10:59 +0100

    replace zip logic

--- src/day06.rs
+++ src/day06.rs
@@ -1,40 +1,27 @@
 use std::{error::Error, fs::read_to_string};
 
-macro_rules! zip {
-    ($x: expr) => ($x);
-    ($x: expr, $($y: expr), +) => (
-        $x.iter().zip(
-            zip!($($y), +))
-    )
-}
-
 pub fn part1(file_path: &str) -> Result<String, Box<dyn Error>> {
     let file = read_to_string(file_path)?;
     let lines: Vec<Vec<&str>> = file
         .lines()
         .map(|l| l.split_whitespace().collect())
-        .collect::<_>();
+        .collect();
 
-    let res: u64 = lines[0]
-        .iter()
-        .zip(lines[1].iter())
-        .zip(lines[2].iter())
-        .zip(lines[3].iter())
-        .zip(lines[4].iter())
-        .map(|((((a, b), c), d), op)| {
-            (
-                vec![
-                    a.parse::<u64>().unwrap(),
-                    b.parse::<u64>().unwrap(),
-                    c.parse::<u64>().unwrap(),
-                    d.parse::<u64>().unwrap(),
-                ],
-                op,
-            )
+    let num_cols = lines[0].len();
+    let num_rows = lines.len();
+
+    let res: u64 = (0..num_cols)
+        .map(|col| {
+            let mut nums = vec![];
+            for row in 0..num_rows - 1 {
+                nums.push(lines[row][col].parse::<u64>().unwrap());
+            }
+            let op = lines[num_rows - 1][col];
+            (nums, op)
         })
         .map(|(nums, op)| match op {
-            &"+" => nums.iter().sum::<u64>(),
-            &"*" => nums.iter().product::<u64>(),
+            "+" => nums.iter().sum::<u64>(),
+            "*" => nums.iter().product::<u64>(),
             a => panic!("inval: {a}, inputs: {:?} {:?}", nums, op),
         })
         .sum();
