4eae0598f8b12507c09bfd00079dcec13385098f
Mikulex <mikulex@mikulex.com>
2025-12-11 23:32:36 +0100

    solve 11/2

--- src/day11.rs
+++ src/day11.rs
@@ -6,6 +6,24 @@ use std::{
 
 pub fn part1(file_path: &str) -> Result<String, Box<dyn Error>> {
     let file = read_to_string(file_path)?;
+    let map = get_map(&file);
+    let mut cache: HashMap<&str, u64> = HashMap::new();
+    let sum = build_path("out", &map.get("out").unwrap(), &map, &mut cache);
+
+    Ok(sum.to_string())
+}
+
+pub fn part2(file_path: &str) -> Result<String, Box<dyn Error>> {
+    let file = read_to_string(file_path)?;
+    let map = get_map(&file);
+    let mut cache: HashMap<(String, bool, bool), u64> = HashMap::new();
+    let exit = &map.get("out").unwrap();
+    let sum = build_path_2("out", "svr", exit, false, false, &map, &mut cache);
+
+    Ok(sum.to_string())
+}
+
+fn get_map<'a>(file: &'a String) -> HashMap<&'a str, HashSet<&'a str>> {
     let map: HashMap<&str, HashSet<&str>> = file
         .lines()
         .map(|l| l.split_once(": ").unwrap())
@@ -16,12 +34,41 @@ pub fn part1(file_path: &str) -> Result<String, Box<dyn Error>> {
                 acc
             })
         });
+    map
+}
 
-    let mut cache: HashMap<&str, u64> = HashMap::new();
+fn build_path_2<'a>(
+    exit: &'a str,
+    start: &'a str,
+    next: &HashSet<&'a str>,
+    seen_dac: bool,
+    seen_fft: bool,
+    map: &HashMap<&'a str, HashSet<&'a str>>,
+    cache: &mut HashMap<(String, bool, bool), u64>,
+) -> u64 {
+    let key = (exit.to_string(), seen_dac, seen_fft);
 
-    let sum = build_path("out", &map.get("out").unwrap(), &map, &mut cache);
+    if let Some(n) = cache.get(&key) {
+        *n
+    } else if exit == start {
+        if seen_dac && seen_fft { 1 } else { 0 }
+    } else if next.is_empty() {
+        0
+    } else {
+        let res = next
+            .into_iter()
+            .map(|n| {
+                let seen_dac = if *n == "dac" { true } else { seen_dac };
+                let seen_fft = if *n == "fft" { true } else { seen_fft };
+                let fallback = HashSet::new();
+                let next = map.get(n).unwrap_or(&fallback);
+                build_path_2(n, start, next, seen_dac, seen_fft, map, cache)
+            })
+            .sum();
 
-    Ok(sum.to_string())
+        cache.insert(key, res);
+        res
+    }
 }
 
 fn build_path<'a>(
@@ -30,8 +77,8 @@ fn build_path<'a>(
     map: &HashMap<&str, HashSet<&'a str>>,
     cache: &mut HashMap<&'a str, u64>,
 ) -> u64 {
-    if cache.get(exit).is_some() {
-        *cache.get(exit).unwrap()
+    if let Some(n) = cache.get(exit) {
+        *n
     } else if exit == "you" {
         1
     } else if next.is_empty() {
@@ -47,9 +94,6 @@ fn build_path<'a>(
     }
 }
 
-pub fn part2(_file_path: &str) -> Result<String, Box<dyn Error>> {
-    todo!()
-}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,6 +106,6 @@ mod tests {
 
     fn _test_day11_02() {
         let res = part2("inputs/11/demo.txt").unwrap();
-        assert_eq!(res.to_string(), "");
+        assert_eq!(res.to_string(), "2");
     }
 }
